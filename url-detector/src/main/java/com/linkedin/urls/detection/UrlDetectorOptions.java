ad9b3f26b310b17b5638d6fd3db19c76d052a803
Jem Jensen <jem.jensen@zoom.us>
2022-04-28 16:38:07 -0500

    Renumbering ALLOW_COLON_WITHOUT_SLASHES

--- url-detector/src/main/java/com/linkedin/urls/detection/UrlDetectorOptions.java
+++ url-detector/src/main/java/com/linkedin/urls/detection/UrlDetectorOptions.java
@@ -58,14 +58,14 @@ public enum UrlDetectorOptions {
   HTML(27), //00011011
 
   /**
-   * Checks for URLs without slashes as well. Ex: http:example.com
-   */
-  ALLOW_COLON_WITHOUT_SLASHES(29),
-
-  /**
    * Checks for single level domains as well. Ex: go/, http://localhost
    */
-  ALLOW_SINGLE_LEVEL_DOMAIN(32); //00100000
+  ALLOW_SINGLE_LEVEL_DOMAIN(32), //00100000
+
+  /**
+   * Checks for URLs without slashes as well. Ex: http:example.com
+   */
+  ALLOW_COLON_WITHOUT_SLASHES(119);
 
   /**
    * The numeric value.
