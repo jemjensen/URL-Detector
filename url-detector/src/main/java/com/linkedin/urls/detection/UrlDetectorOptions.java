9fbdb45a3520417e0d2519e37d788eafcee603bb
Jem Jensen <jem.jensen@zoom.us>
2022-04-28 14:50:00 -0500

    Added support for full list of IANA schemes

--- url-detector/src/main/java/com/linkedin/urls/detection/UrlDetectorOptions.java
+++ url-detector/src/main/java/com/linkedin/urls/detection/UrlDetectorOptions.java
@@ -58,6 +58,11 @@ public enum UrlDetectorOptions {
   HTML(27), //00011011
 
   /**
+   * Checks for URLs against the full IANA scheme list for maximum possible detections
+   */
+  EXTENDED_IANA_DETECTION(31),
+
+  /**
    * Checks for single level domains as well. Ex: go/, http://localhost
    */
   ALLOW_SINGLE_LEVEL_DOMAIN(32); //00100000
